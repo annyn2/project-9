5e2301b57f27187e58e89c3c0d407c3a0d3b818b
annyn2 <127265840+annyn2@users.noreply.github.com>
2023-03-07 16:51:48 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box
+function setup() {
+  createCanvas(400,400);
+
+box = createSprite(200,200,10,10);
+box.shapeColor="red"
+
+}
+
+function draw() 
+{
+
+background("blue");
+drawSprites();
+}
+
+
+
+
